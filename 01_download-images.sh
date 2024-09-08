9721496e4b57dab4cd9fb83942c965d33bcd501d
Advik Shreekumar <advikshreekumar@gmail.com>
2024-09-07 21:45:13 -0400

    cut dirs

--- 01_download-images.sh
+++ 01_download-images.sh
@@ -1,2 +1,2 @@
 mkdir -p raw_images
-cat data/PENDING_LAB_NAMES.txt | wget -r -N -c -np -nH --cut-dirs=4 -P raw_images --user adviksh --ask-password -i - --base=https://physionet.org/files/mimic-cxr-jpg/2.1.0/
+cat data/PENDING_LAB_NAMES.txt | wget -r -N -c -np -nH --cut-dirs=7 -P raw_images --user adviksh --ask-password -i - --base=https://physionet.org/files/mimic-cxr-jpg/2.1.0/
